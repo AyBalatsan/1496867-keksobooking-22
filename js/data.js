a88ba38638b016d2de9acb3a7a317570ccc4959d
AyBalatsan <AyBalatsan@yandex.ru>
2021-02-22 11:28:22 +0600

    Организация кода

--- js/data.js
+++ js/data.js
@@ -6,11 +6,10 @@ const nearbyAuthor = [];
 for (let i = 0; i < numberAuthor; i++){
   nearbyAuthor.push(getAuthor())
 }
-console.log(nearbyAuthor);
+
 const numberObjects = 10;
 const nearbyObjects = [];
 
 for (let i = 0; i < numberObjects; i++){
   nearbyObjects.push(getOffer())
 }
-console.log(nearbyObjects);
