332063d840b9a8c54d6b729460b68f2e0bf91c44
AyBalatsan <AyBalatsan@yandex.ru>
2021-03-06 23:09:37 +0600

    Отрисуй меня полностью (часть 1)

--- js/popup.js
+++ js/popup.js
@@ -1,4 +1,5 @@
 import {nearbyAuthor, nearbyObjects} from './data.js';
+
 const mapCanvas = document.querySelector('#map-canvas');
 const card = document.querySelector('#card').content;
 const popup = card.querySelector('.popup');
@@ -12,7 +13,6 @@ const popupFeatures = card.querySelector('.popup__features');
 const popupDescription = card.querySelector('.popup__description');
 const popupPhotos = card.querySelector('.popup__photos');
 const popupAvatar = card.querySelector('.popup__avatar');
-
 const getTypeProperty = function(type){
   switch (type) {
     case 'flat':
@@ -32,19 +32,23 @@ const getTypeProperty = function(type){
       break;
   }
 }
+
 for (let i = 0; i < 1; i++){
+  const clonedElement = popup.cloneNode(true);
+  const typeProperty = nearbyObjects[i].type;
+
+  getTypeProperty(typeProperty);
+
   popupTitle.textContent = nearbyObjects[i].title;
   popupAddress.textContent = nearbyObjects[i].address;
   popupPrice.textContent = nearbyObjects[i].price + ' ₽/ночь';
-  const typeProperty = nearbyObjects[i].type;
-  getTypeProperty(typeProperty);
   popupCapacity.textContent = `${nearbyObjects[i].rooms} комнаты для ${nearbyObjects[i].guests} гостей`;
   popupTime.textContent = `Заезд после ${nearbyObjects[i].checkin}, выезд до ${nearbyObjects[i].checkout}`;
   popupFeatures.textContent = nearbyObjects[i].features;
   popupDescription.textContent = nearbyObjects[i].description;
   popupPhotos.src = nearbyObjects[i].photos;
   popupAvatar.src = nearbyAuthor[i].avatar;
-  const clonedElement = popup.cloneNode(true);
+
   mapCanvas.appendChild(clonedElement);
 }
 
